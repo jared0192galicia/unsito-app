a0477d8cbfdfdaf0f5229567b086da784dceb947
Jared Galicia <jared0192galicia@gmail.com>
2026-01-19 10:28:02 -0600

    wfded

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,4 +1,4 @@
 export const serverAPI =
   process.env.NEXT_PUBLIC_ENVIRONMENT == 'production'
     ? 'https://api.citas.devjared.com/'
-    : 'http://localhost:5000/';
+    : 'http://api-unsito.devjared/';
