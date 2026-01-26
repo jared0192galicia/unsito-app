b6d7b153c67f9ec82a84f411ab4de5fafcc40be4
Jared Galicia <jared0192galicia@gmail.com>
2026-01-26 17:15:20 -0600

    chage ssl

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,4 +1,4 @@
 export const serverAPI =
   process.env.NEXT_PUBLIC_ENVIRONMENT == 'production'
-    ? 'https://132.18.38.133:5000/'
+    ? 'http://132.18.38.133:5000/'
     : 'http://localhost:5000/';
